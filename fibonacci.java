dfa3597bca71f64a7ed40e91782862fe091499ac
Manan Gupta <76112002+manan-gupta27@users.noreply.github.com>
2021-01-18 22:09:49 +0530

    Create fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int n1=0;
+        int n2=1;
+        System.out.println(n1+"\n"+n2);
+        for(int i=2;i<n;++i){
+            int n3=n1+n2;
+            System.out.println(n3);
+            n1=n2;
+            n2=n3;
+        }
+    }
+}
